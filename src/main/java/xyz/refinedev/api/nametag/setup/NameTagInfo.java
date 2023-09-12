e9ce1af0fd2c57029836af20591e0e8ff96e04ef
DevDrizzy <fistergaming123@gmail.com>
2023-09-12 21:40:57 +0500

    Fix project setup
    Signed-off-by: DevDrizzy <fistergaming123@gmail.com>

--- src/main/java/xyz/refinedev/api/nametag/setup/NameTagInfo.java
+++ src/main/java/xyz/refinedev/api/nametag/setup/NameTagInfo.java
@@ -1,19 +1,23 @@
 package xyz.refinedev.api.nametag.setup;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-
 import xyz.refinedev.api.nametag.packet.ScoreboardPacket;
 
 import java.util.ArrayList;
 
 @Getter @Setter
-@RequiredArgsConstructor
 public class NameTagInfo {
 
     private final String name;
     private final String prefix;
     private final String suffix;
-    private final ScoreboardPacket teamAddPacket = new ScoreboardPacket(name, prefix, suffix, new ArrayList<>(), 0);
+    private final ScoreboardPacket teamAddPacket;
+
+    public NameTagInfo(String name, String prefix, String suffix) {
+        this.name = name;
+        this.prefix = prefix;
+        this.suffix = suffix;
+        this.teamAddPacket = new ScoreboardPacket(name, prefix, suffix, new ArrayList<>(), 0);
+    }
 }
